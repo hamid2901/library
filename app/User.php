57e4968c7f72a11a3d278c42fe71239e680d2e21
mortezamp <morteza_moradipour@yahoo.com>
2018-11-17 23:50:13 +0330

    Models added but not complete, User functions should be complete.

--- app/User.php
+++ app/User.php
@@ -2,6 +2,11 @@
 
 namespace App;
 
+// morteza use follows. 
+use Illuminate\Database\Eloquent\Model;
+
+
+// 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -27,4 +32,43 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    //morteza write follows.
+
+    /**
+     * Get the personal_data record associated with the user.
+     */
+    public function personal_data()
+    {
+        return $this->hasOne('App\Personal_data', 'user_id');
+    }
+
+    
+    /**
+     * The books that belong to the User.
+     */
+
+    public function books()
+    {
+        return $this->belongsToMany('App\Book','book_factor_user', 'user_id', 'book_id');
+    }
+
+    
+      /**
+     * Get the user_roles record associated with the user.
+     */
+    public function user_roles()
+    {
+        return $this->belongsTo('App\User_role', 'role_id');
+    }
+
+    
+      /**
+     * Get the user_status record associated with the user.
+     */
+    public function user_status()
+    {
+        return $this->belongsTo('App\User_status', 'status_id');
+    }
+
 }
