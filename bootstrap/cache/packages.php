f2a743127bfef2a342603565f420ec96080a4dd3
hamidreza <hrda2901@yahoo.com>
2018-11-25 23:36:09 +0330

    the gitignore! :/

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,111 @@
+<?php return array (
+  'beyondcode/laravel-dump-server' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BeyondCode\\DumpServer\\DumpServerServiceProvider',
+    ),
+  ),
+  'brackets/admin-auth' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Brackets\\AdminAuth\\AdminAuthServiceProvider',
+    ),
+  ),
+  'brackets/admin-generator' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Brackets\\AdminGenerator\\AdminGeneratorServiceProvider',
+    ),
+  ),
+  'brackets/admin-listing' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Brackets\\AdminListing\\AdminListingServiceProvider',
+    ),
+  ),
+  'brackets/admin-ui' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Brackets\\AdminUI\\AdminUIServiceProvider',
+    ),
+  ),
+  'brackets/translatable' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Brackets\\Translatable\\TranslatableServiceProvider',
+    ),
+  ),
+  'digitaldream/laracrud' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'LaraCrud\\LaraCrudServiceProvider',
+    ),
+    'aliases' => 
+    array (
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'krlove/eloquent-model-generator' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Krlove\\EloquentModelGenerator\\Provider\\GeneratorServiceProvider',
+    ),
+  ),
+  'laravel/scout' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Scout\\ScoutServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'spatie/laravel-translatable' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Translatable\\TranslatableServiceProvider',
+    ),
+  ),
+  'xethron/migrations-generator' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Way\\Generators\\GeneratorsServiceProvider',
+      1 => 'Xethron\\MigrationsGenerator\\MigrationsGeneratorServiceProvider',
+    ),
+  ),
+);
